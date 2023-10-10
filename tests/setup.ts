bbc118f4b382ecfbcfa2e1b273978eedc87b6608
odrobcha <drobchak.o@gmail.com>
2023-10-10 13:00:30 +0200

    fix: add matchers as matchers in tsconfig.ts

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,9 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/vue";
 import {expect, afterEach } from "vitest";
-// import matchers from "@testing-library/jest-dom/matchers";
-//
-// expect.extend(matchers);
+import * as matchers from '@testing-library/jest-dom/matchers';
+
+expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
